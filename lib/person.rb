2761737c29bdd479c04162a70479ed4c9c571a5b
little-Dave <17536424+little-Dave@users.noreply.github.com>
2019-06-11 13:24:09 -0600

    Define two Ruby classes, each with methods that read/write instance variables
    Define two classes, Person and Dog. Define get and set methods that read from and write to instance variables for each

---
+++ lib/person.rb
@@ -0,0 +1,19 @@
+class Person
+
+  def name=(persons_name)
+    @name = persons_name
+  end
+
+  def name
+    @name
+  end
+
+  def job=(a_job)
+    @job = a_job
+  end
+
+  def job
+    @job
+  end
+
+end
